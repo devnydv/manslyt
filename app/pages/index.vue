10a80ff81e4a49432d8609116fd5b85b704a7a90
devnydv <bittumailb2@gmail.com>
2025-10-26 11:56:09 +0530

    test

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,18 @@
 <template>
-    this is home page
+    <div>
+        <h1>Posts</h1>
+        <ul>
+            <li v-for="post in data" :key="post.id">{{ post.title }}</li>
+        </ul>
+    </div>
 </template>
+<script setup>
+
+const { data, error } = useFetch('https://jsonplaceholder.typicode.com/posts')
+if (error.value) {
+    
+    console.error('Error fetching posts:', error.value)
+} else {
+    console.log('Fetched posts:', data.value)
+}
+</script>

4b3511a7644070fcc2e2f02f04d83533047e8e59
devnydv <bittumailb2@gmail.com>
2025-10-26 12:48:15 +0530

    news fetch

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,14 +5,21 @@
     </nav>
     <div>
         <h1>Posts</h1>
-        <ul>
-            <li v-for="post in data" :key="post.id">{{ post.title }}</li>
-        </ul>
+        
+        <div class="card" v-for="post in data" :key="post.id">
+            <img :src="post.imageUrl" alt="">
+            <div id="content">
+            <h2>{{ post.title }}</h2>
+            
+            <p>{{ post.content }}</p>
+            </div>
+        
+        </div>
     </div>
 </template>
 <script setup>
 
-const { data, error } = useFetch('https://jsonplaceholder.typicode.com/posts')
+const { data, error } = useFetch('https://filmyapp-e1005.firebaseio.com/news/all/data.json?orderBy="$key"&limitToLast=24')
 
 useHead({
     htmlAttrs: {
@@ -24,3 +31,35 @@ useHead({
     ]
 })
 </script>
+
+<style scoped>
+
+.card {
+    max-width: 80%;
+    margin-bottom: 20px;
+    margin-left: auto;
+    margin-right: auto;
+    display: flex;
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    padding: 16px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    background-color: #e2e1e1;
+}
+img {
+    max-width: 300px;
+    max-height: 300px;
+    padding: 20px;
+    
+
+}
+h1 {
+    text-align: center;
+}
+h2 {
+    text-align: justify;
+}
+p {
+    
+}
+</style>

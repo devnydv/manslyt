dffcc2e90961c22ffcd5933fa15207d114335ceb
devnydv <bittumailb2@gmail.com>
2025-10-31 00:35:00 +0530

    slide change

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -23,9 +23,9 @@ useHead({
     htmlAttrs: {
         lang: 'en'
     },
-    title: 'Home Page',
+    title: 'the contact Page',
     meta: [
-        { name: 'description', content: 'This is the home page of our Nuxt 3 application.' }
+        { name: 'description', content: 'This is the contact page of our Nuxt 3 application.' }
     ]
 })
 
